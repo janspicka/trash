1dd07cfe679223e32a92cdc7a922e887bf8f0082
Jan Špička <spicka.jan@gmail.com>
2015-07-23 14:31:01 +0200

    Update plsql_notes.sql

--- plsql/plsql_notes.sql
+++ plsql/plsql_notes.sql
@@ -2,33 +2,70 @@ set serveroutput on;
 
 DECLARE
 -- GLOBAL
-  v_text varchar2(100);
-  v_text2 varchar2(100) := 'Hello world!';
-  v_bool boolean := false; 
-  v_number number(5);
-  
-  v_fname employees.first_name%TYPE;
-  -- v_fname varchar(100);
+v_result varchar2(100);
+v_num number(5);
+v_id number(5);
+i number(5);
+
+f_name employees.first_name%TYPE;
+l_name employees.last_name%TYPE;
 
 BEGIN 
+
+  -- CASE 
+  v_id := 120;
+  case 
+  when v_id is not null then 
+    select first_name, last_name into f_name, l_name from employees where employee_id = v_id;
+    v_result := f_name || ' ' || l_name;
+  else 
+    v_result := 'Nema nadrizeneho.';
+  end case;
+  dbms_output.put_line(v_result);
+  
+  -- LOOP
+  i := 100;
+  loop
+    exit when i > 120;
+    i := i + 2;
+    select first_name, last_name into f_name, l_name from EMPLOYEES where employee_id = i;
+    dbms_output.put_line(i || ' ' ||  f_name || ' ' || l_name);
+  end loop;
+  
+  -- WHILE
+  i := 100;
+  while i < 150 loop
+     if i > 120 then 
+      exit;
+    end if;
+    dbms_output.put_line(i);
+      i := i + 1;
+  end loop;
+  
+  -- FOR
+  for id in 100..115 loop
+    select first_name, last_name into f_name, l_name from employees where employee_id = id;
+    dbms_output.put_line(id || ' ' ||  f_name || ' ' || l_name);
+  end loop;
+ 
+   -- FOR
+  for id in 0..20 loop
+    -- skip even number
+    continue when mod(id,2) = 1;
+    dbms_output.put_line(id);
+  end loop;
+
+      
+
+
   DECLARE
   -- LOCAL
   BEGIN
+    -- minimal code
     null;
-    v_text := 'Hello world!';
-    -- dbms_output.put_line(v_text);
-    -- dbms_output.put_line(v_text2);
-    -- 1 hodina = 1
-    -- dbms_output.put_line(to_char(sysdate-3/24, 'DD-MM-YYYY HH:MI:SS'));
-    -- v_number := '3.14';
-    v_number := '3,14';
-    -- dbms_output.put_line(v_number);
   END;
-  
-  
-  
-  SELECT first_name INTO v_fname FROM employees WHERE employee_id = 200;
-  dbms_output.put_line('First Name is : '||v_fname);
-
 END;
 /
+
+
+select mod(10,5) from dual;
