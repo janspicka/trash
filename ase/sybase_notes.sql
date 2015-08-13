8e5f1c28aa0ac0269aaf4b763fa1f635f4d04c21
Jan Špička <spicka.jan@gmail.com>
2015-08-13 15:01:58 +0200

    Update sybase_notes.sql

--- ase/sybase_notes.sql
+++ ase/sybase_notes.sql
@@ -1,1 +1,15 @@
+# promenne v prostredi  
+. skript.sh
+env 
 
+
+
+# neco s ase 
+
+
+cd /u01/app/sybase/SYB15.7
+. SYBASE.sh 
+env | grep SYBASE
+
+cd $SYBASE/$SYBASE_ASE/install 
+startserver -r 
