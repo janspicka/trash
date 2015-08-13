ada20ac9f1c52019924f3096f9315984bf242dab
Jan Špička <spicka.jan@gmail.com>
2015-08-13 15:30:44 +0200

    Update sybase_notes.sql

--- ase/sybase_notes.sql
+++ ase/sybase_notes.sql
@@ -2,8 +2,6 @@
 . skript.sh
 env 
 
-
-
 # neco s nastartovanim engine
 cd /u01/app/sybase/SYB15.7
 . SYBASE.sh 
@@ -14,3 +12,16 @@ startserver -f RUN_LNX_ASE157
 # jsem ready? 
 showserver
 which showserver
+
+# pripojeni 
+cd $SYBASE
+cat interfaces
+isql -Usa -SLNX_ASE157 -w1000
+
+# nejake prikazy
+
+select $$servername
+go 
+
+select * from master.dbo.sysdatabases
+go
