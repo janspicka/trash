bb628924304eecef807babf6ec86e99e60758a82
Jan Špička <spicka.jan@gmail.com>
2015-08-13 15:15:44 +0200

    Update sybase_notes.sql

--- ase/sybase_notes.sql
+++ ase/sybase_notes.sql
@@ -4,12 +4,13 @@ env
 
 
 
-# neco s ase 
-
-
+# neco s nastartovanim engine
 cd /u01/app/sybase/SYB15.7
 . SYBASE.sh 
 env | grep SYBASE
-
 cd $SYBASE/$SYBASE_ASE/install 
-startserver -r 
+startserver -f RUN_LNX_ASE157
+
+# jsem ready? 
+showserver
+which showserver
